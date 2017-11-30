43a78822915b44a285e381820c659e0c06ec5def
Zorca <vs@zorca.org>
2017-11-30 17:39:36 +0300

    Check existing assets

--- htdocs/content/themes/theme/resources/autoload/assets.php
+++ htdocs/content/themes/theme/resources/autoload/assets.php
@@ -1,7 +1,11 @@
 <?php
 
-// Load a CSS file stored in dist/css/screen.css
-Asset::add('main-style', 'styles/main.css', false, '1.0', 'all');
+if (file_exists(themosis_path('theme').'/dist/styles/main.css')) {
+    // Load a CSS file stored in dist/css/screen.css
+    Asset::add('main-style', 'styles/main.css', false, '1.0', 'all');
+}
 
-// Load a JS file stored in dist/js/main.js
-Asset::add('main-script', 'scripts/main.js', ['jquery'], '1.0', true);
+if (file_exists(themosis_path('theme').'/dist/scripts/main.js')) {
+    // Load a JS file stored in dist/js/main.js
+    Asset::add('main-script', 'scripts/main.js', ['jquery'], '1.0', true);
+}
